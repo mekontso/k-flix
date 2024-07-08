cbbf48c3106a2678e7e2ac5aa81e29980dc9c1ab
mekontso <mekontsoanderson@gmail.com>
2024-07-08 17:45:29 +0100

    Genre form

--- src/app/(admin)/admin/genre/create/page.tsx
+++ src/app/(admin)/admin/genre/create/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import GenreForm from "@/components/admin/genre/GenreForm";
+import BreadcrumbCustom from "@/components/global/BreadcrumbCustom";
 
 const Page: React.FC = () => {
-    return (
-        <div className="flex items-center justify-center h-full">
-    <GenreForm/>
-    </div>)
+    const breadcrumbLinks = [{href: "/admin", label: "Dashboard"}, {href: "/admin/genre", label: "Genre"}];
+    const currentPageName = "Create Genre";
+    return (<>
+            <div className="mb-8"><BreadcrumbCustom links={breadcrumbLinks} currentPageName={currentPageName}/></div>
+            <div className="flex items-center justify-center h-full">
+                <GenreForm/>
+            </div>
+        </>
+
+    )
 }
 
 export default Page;
--- src/app/(admin)/admin/genre/page.tsx
+++ src/app/(admin)/admin/genre/page.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import {Plus} from "lucide-react";
+import BreadcrumbCustom from "@/components/global/BreadcrumbCustom";
 
 const Page: React.FC = () => {
+    const breadcrumbLinks = [{ href: "/admin", label: "Dashboard" }];
+    const currentPageName = "Genre";
     return (<div>
+        <div className="mb-8"><BreadcrumbCustom links={breadcrumbLinks} currentPageName={currentPageName}/></div>
         <div className="flex justify-between">
             <h1>Genre</h1>
-            <Button >
+            <Button>
                 <Link href="/admin/genre/create" className="flex items-center">
                     <Plus/>
                     <span className="ml-2">New</span>
